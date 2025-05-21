e93f4d40c8c4750ac24ee4a3357ac8b21e996d03
Emiliano Portillo <emiliano.portillo1331@gmail.com>
2025-05-20 21:07:07 -0600

    feat: Header was added in home page

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,41 +1,29 @@
-'use client';
+"use client";
+import Header from "@/components/Home/Header";
 import OptionCard from "@/components/Home/OptionCard";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="flex gap-12">
-        <h1 className="text-4xl font-bold mb-6 text-white">
-          ¡Bienvenido, {"usuario"}!
-        </h1>
-      </div>
-      <div className="flex flex-col gap-8 items-center w-[80vh] h=[100vh]">
-        <div className="flex flex-row gap-8">
-          <OptionCard
-            color="red"
-            title="¿Cómo te sientes hoy?"
-            to=""
-          />
-          <OptionCard
-            color="yellow"
-            title="Mi Psicólogo virtual"
-            to=""
-          />
+    <>
+      <Header userName="Nombre de ejemplo..."/>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <div className="flex gap-12">
+          <h1 className="text-4xl font-bold mb-6 text-white">
+            ¡Bienvenido, {"usuario"}!
+          </h1>
         </div>
-        <div className="flex flex-row gap-8">
-          <OptionCard
-            color="purple"
-            title="Ayuda profesional"
-            to=""
-          />
-          <OptionCard 
-            color="green" 
-            title="Mi día a día" 
-            to=""
-            />
+        <div className="flex flex-col gap-8 items-center w-[80vh] h=[100vh]">
+          <div className="flex flex-row gap-8">
+            <OptionCard color="red" title="¿Cómo te sientes hoy?" to="" />
+            <OptionCard color="yellow" title="Mi Psicólogo virtual" to="" />
+          </div>
+          <div className="flex flex-row gap-8">
+            <OptionCard color="purple" title="Ayuda profesional" to="" />
+            <OptionCard color="green" title="Mi día a día" to="" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Home;
