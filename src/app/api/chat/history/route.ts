98ad485407a4eb4621f9094ea2433aafbce5770b
Emiliano Portillo <emiliano.portillo1331@gmail.com>
2025-05-22 19:33:16 -0600

    Fix: Configurations to deploy

--- src/app/api/chat/history/route.ts
+++ src/app/api/chat/history/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import prisma from "@/libs/db";
 import { cookies } from "next/headers";
 import { jwtVerify } from "jose";
-import { Chat } from "@/types/chat";
 
 const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
@@ -10,7 +9,6 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const chatId = searchParams.get("chatId");
 
-  // Si hay chatId, devuelve los mensajes de ese chat
   if (chatId) {
     const messages = await prisma.message.findMany({
       where: { chatId },
@@ -20,7 +18,7 @@ export async function GET(request: Request) {
     return NextResponse.json({ messages });
   }
 
-  // Si NO hay chatId, devuelve el historial de chats del usuario autenticado
+
   const token = (await cookies()).get("Auth_psiq")?.value;
   if (!token) return NextResponse.json({ chats: [] });
 
@@ -39,7 +37,7 @@ export async function GET(request: Request) {
   });
 
   return NextResponse.json({
-    chats: chats.map((chat: Chat) => ({
+    chats: chats.map((chat: any) => ({
       id: chat.id,
       firstMessage: chat.messages[0]?.text ?? "(Sin mensajes)",
       createdAt: chat.createdAt,
